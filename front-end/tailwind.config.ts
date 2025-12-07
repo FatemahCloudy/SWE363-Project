7947ce4f0084efbf7c1d30f86371b9ea342a2074
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-12-07 20:08:55 +0300

    Update tailwind.config.ts

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -88,6 +88,6 @@ module.exports = {
     },
     container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  //plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
   darkMode: ["class"],
 };
